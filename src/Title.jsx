4e3ff1cea3eab40fc508a58df7f90b14ded24698
jo4ovictor <joaovictordeaguiarmoura@gmail.com>
2022-06-23 11:02:15 -0400

    Adicionando componente Title

---
+++ src/Title.jsx
@@ -0,0 +1,11 @@
+import "./Title.css"
+
+function Title() {
+    return(
+    <div>
+        <p className="title">NOTÍCIAS</p>
+        <p className="subtitle">PORTAL DO IFRO</p>
+    </div>
+    );
+}
+export default Title;
